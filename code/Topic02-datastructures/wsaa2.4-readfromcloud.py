0eb24036deb261c2e782336dad18157d9bd3d00c
Andrew Beatty <andrew.beatty@gmit.ie>
2024-01-31 19:39:12 +0000

    week02

--- code/Topic02-datastructures/wsaa2.4-readfromcloud.py
+++ code/Topic02-datastructures/wsaa2.4-readfromcloud.py
@@ -3,3 +3,12 @@ import json
 
 url = "https://api.coindesk.com/v1/bpi/currentprice.json"
 response = requests.get(url)
+#print (response.json())
+data = response.json()
+#with open ("bitcoindump.json", "w") as fp:
+#    json.dump(data, fp)
+
+bpi = data["bpi"]
+#print(bpi)
+rate = bpi["EUR"]["rate"]
+print(rate)
--- code/Topic02-datastructures/wsaa2.4-readjson.py
+++ code/Topic02-datastructures/wsaa2.4-readjson.py
@@ -1,8 +1,9 @@
 import json
-filename = "dr2.4-json.json"
+filename = "wsaa2.4-json.json"
 
 with open(filename, "r") as fp:
     jsonobject = json.load(fp)
 #print (jsonobject)
-#for employee in jsonobject["employees"]:
-#    print(employee)
+for employee in jsonobject["employees"]:
+    print(employee["firstName"])
+
